9f73f4d6b465b307c65f13efef9f37962e7d39a7
yudady <yu_dady@yahoo.com.tw>
2017-02-15 21:53:19 +0800

    pic

---
+++ src/main/java/com/zonvan/Teso02.java
@@ -0,0 +1,39 @@
+package com.zonvan;
+
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class Teso02 {
+
+	public final static void scale(String srcImageFile, String result, int scale, boolean flag) {
+		try {
+			BufferedImage src = ImageIO.read(new File(srcImageFile)); // 读入文件
+			int width = src.getWidth(); // 得到源图宽
+			int height = src.getHeight(); // 得到源图长
+			if (flag) {// 放大
+				width = width * scale;
+				height = height * scale;
+			} else {// 缩小
+				width = width / scale;
+				height = height / scale;
+			}
+			Image image = src.getScaledInstance(width, height, Image.SCALE_DEFAULT);
+			BufferedImage tag = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+			Graphics g = tag.getGraphics();
+			g.drawImage(image, 0, 0, null); // 绘制缩小后的图
+			g.dispose();
+			ImageIO.write(tag, "JPEG", new File(result));// 输出到文件流
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
